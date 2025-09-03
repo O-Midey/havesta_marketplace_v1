499e22e26c1d3b659fdcfe06772946a3d7b46a7c
ShyAnne <shyannetech1@gmail.com>
2025-09-03 09:12:23 +0100

    whoosh

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import appstore from "../assets/App Store.svg";
+import playstore from "../assets/Play Store.svg";
+import phones from "../assets/phones.svg";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="bg-[#01BE724D] flex flex-col py-10 px-16">
+      <div className="m-auto flex items-center max-w-7xl w-full justify-center">
+        {/* text area */}
+        <div className="">
+          <h1 className="text-[#000000] font-bold text-5xl max-w-lg mb-6 ">
+            Shop Faster With Havesta App
+          </h1>
+          <p className="font-semibold text-lg max-w-sm leading-relaxed mb-10">
+            Get fresh farm produce delivered in minutes from a variety of
+            trusted vendors{" "}
+          </p>
+
+          {/* stores */}
+          <div className="flex  gap-4 ">
+            <img
+              src={appstore}
+              alt=""
+              className="cursor-pointer transform transition duration-300 hover:scale-110"
+            />
+            <img
+              src={playstore}
+              alt=""
+              className="cursor-pointer transform transition duration-300 hover:scale-110"
+            />
+          </div>
+        </div>
+
+        {/* phones */}
+        <div className=" w-[40%]">
+          <img src={phones} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
